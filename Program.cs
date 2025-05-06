bfa18c516db6162a0c0f16d7b8bf0d8b2d5e4edb
gabs1san <sanbiel398@gmail.com>
2025-05-06 19:44:09 -0300

    Instanciando os Objetos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Compiscao;
+using Compiscao.Entities;
+using Compiscao.Entities.Enums;
+using System;
+using System.Globalization;
+
 
 namespace Composicao
 {
@@ -6,6 +11,22 @@ namespace Composicao
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter departments name: ");
+            string deptName = Console.ReadLine();
+
+            Console.Write("Enter Worker data:");
+            Console.Write("Name");
+            string name = Console.ReadLine();
+
+            Console.Write("Level (Junir/Pleo/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+
+            Console.WriteLine("BaseSalary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvarianteCulture);
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
 
 
         }
